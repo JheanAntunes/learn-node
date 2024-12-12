80063e98ad599b1b2f4c391689fdd7e9f0db2f70
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-12 13:04:31 -0300

    feat: add class abstract DioAccount

---
+++ src/class/DioAccount.ts
@@ -0,0 +1,31 @@
+export abstract class DioAccount {
+  private name: string = "";
+  private accountNumber: number = 0;
+  private balance: number = 0;
+  private status: boolean = true;
+  constructor(name: string, accountNumber: number) {
+    this.name = name;
+    this.accountNumber = accountNumber;
+  }
+
+  setName = (name: string) => (this.name = name);
+
+  getName = () => this.name;
+
+  deposit = () => {
+    if (this.validateStatus()) {
+      console.log("Depositado");
+    }
+  };
+
+  withdraw = () => {
+    console.log("Sacou ");
+  };
+
+  getBalance = () => console.log("balance: ", this.balance);
+
+  private validateStatus = () => {
+    if (!this.status) throw new Error();
+    return this.status;
+  };
+}
