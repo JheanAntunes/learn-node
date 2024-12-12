edc56b9b7e0e71d4c46ad9d1ed59f618be1a60e6
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-12 13:02:54 -0300

    docs: abstração, herança, polimorfismo

---
+++ src/pol/doc-pol.ts
@@ -0,0 +1,10 @@
+// abstração é a capacidade de conseguir pesar no objeto iremos criar e como o implemtar eus métdos e atributos.
+
+// Herança permite que uma classe compartilhe caracteristacs com outra é usada com objetivo de evitar repetição, reaprveitamneto de código.
+
+// Polimorfismo permite que uma classe filha chame um m´pethodo com assinatura diferente, ou seja, o fluxo do método é dirente quando comparado original.
+
+//Dio Banking
+
+// name, accoutnUMBER
+// methods > depositar, sacar
